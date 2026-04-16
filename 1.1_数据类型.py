31dc96c828cb2863267449be2535b250fc1e25da
yunqiqiu7-cmd <yunqiqiu7@gmail.com>
2026-04-16 18:19:32 +0800

    Add files via upload

---
+++ 1.1_数据类型.py
@@ -0,0 +1,55 @@
+import sys
+
+#NP1.print("Hello World")
+
+#NP2.多数输出
+#print("Hello World")
+#print('Hello Nowcoder')
+#print("Hello World", "Hello Nowcoder", sep="\n")
+
+#NP3.读入字符串
+#print(input())
+
+#NP4.读入整数
+#print(int(input()))
+
+#NP5.格式化输出
+#print(f"Hello {input()}")
+
+#NP6.小数输出
+#print(f"{float(input()):.2f}")
+
+#NP7.小数化整数
+#print(int(float(input())))
+
+#NP8.整数化小数
+#print(f"{(int(input())):.1f}")
+
+#NP9. 十六进制数字的大小
+#print(int(input(), 16))
+
+#NP10. print(input()+input())
+
+#NP11.print(len(input()))
+
+#NP12. 格式化输出（大小写）
+#name = 'niuNiu'
+#print(name.upper())
+#print(name.lower())
+#print(name.title())
+
+#NP13. 去掉字符串两端的空格
+#print(input().strip())
+#print(input().lstrip())
+#print(input().rstrip())
+
+#NP15. 字符串切片
+#print(input()[:10])
+
+#NP16. 列表
+#offer_list = ['A','B']
+#for a in offer_list:
+#    print(f"{a}, Approval!")
+#offer_list[-1] = 'C'
+#for a in offer_list:
+#    print(f"{a}, Welcome!")
